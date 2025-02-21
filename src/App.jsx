4d638d618d5babb4fdb8f953c50c3c895e7a0227
ViharDuggi2005 <suji20021@gmail.com>
2025-02-21 23:44:58 +0530

    Level1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import LevelSelection from "./pages/Levels"; // Ensure this file exists
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LevelSelection from "./pages/Levels";
 import Level1 from "./pages/Level1";
 import Level2 from "./pages/Level2";
 import Level3 from "./pages/Level3";
@@ -11,10 +11,10 @@ import Level5 from "./pages/Level5";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#070625] via-40% to-[#3D348E] text-white">
-      <Header />
-      <div className="grow">
-        <Router>
+    <Router> {/* 🛠 Move Router to wrap the entire app */}
+      <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#070625] via-40% to-[#3D348E] text-white">
+        <Header />
+        <div className="grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/levels" element={<LevelSelection />} />
@@ -24,10 +24,10 @@ const App = () => {
             <Route path="/game/4" element={<Level4 />} />
             <Route path="/game/5" element={<Level5 />} />
           </Routes>
-        </Router>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 };
 
