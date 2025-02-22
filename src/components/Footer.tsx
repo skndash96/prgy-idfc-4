273531df20bfccaa89180c090b19ee9ada97a987
Naveen <naveen@Naveens-MacBook-Pro.local>
2025-02-22 10:34:16 +0530

    l6

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-end mr-4">
-      <p className="mb-2 text-sm text-white font-light font-paytone">
+      <p className="mb-2 text-sm text-white font-light font-paytone hover:cursor-pointer hover:underline">
         Privacy Policy | Terms and Conditions 
       </p>
     </footer>
